2f46652d2a8e84319d8923390f8354c1e87a967d
Daviichii89 <davichi89@gmail.com>
2022-05-01 21:40:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+import Inicio from "./components/Inicio";
+import Contacto from "./components/Contacto";
+import Nosotros from "./components/Nosotros";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container mt-5">
+        <div className="btn-group">
+          <Link to="/" className="btn btn-dark">
+            Inicio
+          </Link>
+          <Link to="/contacto" className="btn btn-dark">
+            Contacto
+          </Link>
+          <Link to="/nosotros" className="btn btn-dark">
+            Nosotros
+          </Link>
+        </div>
+        <hr/>
+        <Switch>
+          <Route path="/contacto" component={<Contacto />}>
+        
+          </Route>
+          <Route path="/nosotros" component={<Nosotros />}>
+            
+          </Route>
+          <Route path="/" exact component={<Inicio />}>
+           
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import {render} from 'testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it('renders content', () => {
+    const app = {
+        content: 'This is a test',
+        important: true
+    }
+
+})
+
+
+
+
+
+
+export default App.test
